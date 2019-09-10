8a34a5620fa4f37f074c397891cf0585170be460
Sean Dyon <sean.dyon@outlook.com>
2019-09-10 13:10:07 -0400

    moving install process from dockerfile to script

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,9 @@
 FROM mcr.microsoft.com/dotnet/framework/aspnet:latest
 
 ARG temp_dir
-ENV temp_dir ${temp_dir:-/temp}
+ENV temp_dir ${temp_dir:-C:\temp}
+ARG entrypoint_dir
+ENV entrypoint_dir ${entrypoint_dir:-C:\entrypoint}
 ARG sisu_version
 ENV sisu_version ${sisu_version:-2.1.24.0}
 ARG sisu
@@ -24,37 +26,14 @@ ENV sa_key ${sa_key}
 
 SHELL ["powershell", "-Command", "$ErrorActionPreference = 'Stop'; $ProgressPreference = 'SilentlyContinue';"]
 
-RUN mkdir $env:temp_dir
+RUN mkdir $env:temp_dir; `
+      mkdir $env:entrypoint_dir
 # define the path we will symbolic link to represent D:
-RUN mkdir /data
+#RUN mkdir /data
+VOLUME [ "C:\\data" ]
 RUN Set-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager\DOS Devices' -Name 'D:' -Value "\??\C:\data" -Type String;
 
-COPY ["idp.zip", "/temp"]
-# copy remote build files and clean up
-RUN Expand-Archive -LiteralPath $env:temp_dir\idp.zip -DestinationPath $env:temp_dir; `
-      Move-Item -Path $env:temp_dir\DRIVE_C\Scripts -Destination C:\Scripts; `
-      Move-Item -Path $env:temp_dir\DRIVE_D\* -Destination C:\data; `
-      Move-Item -Path $env:temp_dir\SecureAuth-Idp-Setup-Utility-$env:sisu_version-Installer.exe -Destination C:\data\MFCApp_BIN\SISU\SISUInstaller.exe; `
-      Get-ChildItem $env:temp_dir -Recurse | ForEach ($_) {Remove-Item $_.fullname -Recurse}
-
-# replace strings in build files
-RUN (get-content $env:base_install_path).replace('Set /P BuildType=Is this a Production Appliance Image build (i.e. Sysprep should be run) [Y or N]?', 'Set BuildType=Y')|Set-Content $env:base_install_path; `
-      (get-content $env:base_install_path).replace('PAUSE &:: ANSIBLE_REPLACE_PAUSE', ':: PAUSE')|Set-Content $env:base_install_path; `
-      (get-content $env:first_install_path).replace('::ANSIBLE_COND1', 'call :Cond_1')|set-content $env:first_install_path; `
-      (get-content $env:first_install_path).replace('Powershell &:: ANSIBLE_REMOVE', ':: Powershell')|Set-Content $env:first_install_path; `
-      (get-content C:\Scripts\Setup\Security\$env:lanmanbypass).replace('NET STOP LanmanServer', 'NET STOP LanmanServer /y')|set-content C:\Scripts\Setup\Security\$env:lanmanbypass
-
-# run hardening scripts then clean up
-RUN & "$env:first_install_path"; `
-      Get-ChildItem C:\Scripts -Recurse | ForEach ($_) {Remove-Item $_.fullname -Recurse}; `
-      Get-ChildItem C:\data\Scripts -Recurse | ForEach ($_) {Remove-Item $_.fullname -Recurse}; `
-      Remove-Item C:\Scripts -Recurse -Force; `
-      Remove-Item C:\data\Scripts -Recurse -Force; `
-      Remove-Item C:\data\MFCApp_Bin\FirstStartup*.cmd; `
-      Remove-Item C:\data\MFCApp_Bin\*.vbs; `
-      Remove-Item C:\data\MFCApp_Bin\*.reg; `
-      Get-ChildItem C:\data\MFCApp_Bin\Startmenu -Recurse | ForEach ($_) {Remove-Item $_.fullname -Recurse}; `
-      Remove-Item C:\data\MFCApp_Bin\Startmenu -Recurse -Force
+COPY ./idp.zip ${temp_dir}
 
 # required for ARR rules with SA Cloud
 RUN Invoke-WebRequest http://download.microsoft.com/download/E/9/8/E9849D6A-020E-47E4-9FD0-A023E99B54EB/requestRouter_amd64.msi -UseBasicParsing -OutFile C:/requestrouter.msi; `
@@ -62,10 +41,7 @@ RUN Invoke-WebRequest http://download.microsoft.com/download/E/9/8/E9849D6A-020E
       Invoke-WebRequest http://download.microsoft.com/download/C/9/E/C9E8180D-4E51-40A6-A9BF-776990D8BCA9/rewrite_amd64.msi -UseBasicParsing -OutFile C:/rewrite.msi; `
       Start-Process msiexec -ArgumentList '/i C:\rewrite.msi /qn' -Wait; rm C:\rewrite.msi
 
-# run SISU installer, then run SA final install
-RUN & "C:\data\MFCApp_Bin\SISU\SISUInstaller.exe"
-
 # Start Script checks if IdP is installed, then runs W3SVC
-COPY ["SecureAuth_Run.ps1", "/temp"]
-ENTRYPOINT ["powershell", "-ExecutionPolicy", "ByPass", "-File", "./temp/SecureAuth_Run.ps1"]
+COPY ./SecureAuth_Run.ps1 ${entrypoint_dir}
+ENTRYPOINT powershell.exe -ExecutionPolicy ByPass -File $env:entrypoint_dir/SecureAuth_Run.ps1
 EXPOSE 80 443
--- SecureAuth_Run.ps1
+++ SecureAuth_Run.ps1
@@ -2,6 +2,72 @@
 
 # check if debug.log exists, if not install IdP
 if(!(Test-Path 'C:\Debug.log' -PathType Leaf))  {
+
+    if(!(Test-Path 'C:\data\Secureauth\SecureAuth0\web.config' -PathType Leaf)) {
+        # set permissions so that cleanup will work
+        Get-ChildItem 'C:\data' -Recurse | ForEach-Object ($_) {
+            $acl = Get-Acl -Path $_.fullname
+            if(($_).GetType().Name -eq 'DirectoryInfo') {
+                $accessrule = New-Object System.Security.AccessControl.FileSystemAccessRule ('Everyone', 'FullControl', 'ContainerInherit, ObjectInherit', 'None', 'Allow')
+                $acl.SetAccessRule($accessrule)
+                Set-Acl -Path $_.fullname -AclObject $acl
+            }
+            # permissions set are slightly different for files than directories
+            if(($_).GetType().Name -eq 'FileInfo') {
+                $accessrule = New-Object System.Security.AccessControl.FileSystemAccessRule ('Everyone', 'FullControl', 'ContainerInherit, ObjectInherit', 'InheritOnly', 'Allow')
+                $acl.SetAccessRule($accessrule)
+                Set-Acl -Path $_.fullname -AclObject $acl
+            }
+        }
+
+        # copy remote build files and clean up
+        Expand-Archive -LiteralPath $env:temp_dir\idp.zip -DestinationPath $env:temp_dir
+        Copy-Item -Path $env:temp_dir\DRIVE_C\Scripts -Destination C:\Scripts -Recurse -Force
+        Copy-Item -Path $env:temp_dir\DRIVE_D\* -Destination C:\data -Recurse -Force
+        Copy-Item -Path $env:temp_dir\SecureAuth-Idp-Setup-Utility-$env:sisu_version-Installer.exe -Destination C:\data\MFCApp_BIN\SISU
+        Rename-Item -Path C:\data\MFCApp_BIN\SISU\SecureAuth-Idp-Setup-Utility-$env:sisu_version-Installer.exe -NewName 'SISUInstaller.exe'
+        Get-ChildItem $env:temp_dir -Recurse | ForEach-Object ($_) {Remove-Item $_.fullname -Recurse}
+
+        # replace strings in build files
+        (get-content $env:base_install_path).replace('Set /P BuildType=Is this a Production Appliance Image build (i.e. Sysprep should be run) [Y or N]?', 'Set BuildType=Y')|Set-Content $env:base_install_path
+        (get-content $env:base_install_path).replace('PAUSE &:: ANSIBLE_REPLACE_PAUSE', ':: PAUSE')|Set-Content $env:base_install_path
+        (get-content $env:first_install_path).replace('::ANSIBLE_COND1', 'call :Cond_1')|set-content $env:first_install_path
+        (get-content $env:first_install_path).replace('Powershell &:: ANSIBLE_REMOVE', ':: Powershell')|Set-Content $env:first_install_path
+        (get-content C:\Scripts\Setup\Security\$env:lanmanbypass).replace('NET STOP LanmanServer', 'NET STOP LanmanServer /y')|set-content C:\Scripts\Setup\Security\$env:lanmanbypass
+    
+        # run hardening scripts then clean up
+        & "$env:first_install_path" | Out-Default
+        
+        # set permissions so that cleanup will work
+        Get-ChildItem 'C:\data' -Recurse | ForEach-Object ($_) {
+            $acl = Get-Acl -Path $_.fullname
+            if(($_).GetType().Name -eq 'DirectoryInfo') {
+                $accessrule = New-Object System.Security.AccessControl.FileSystemAccessRule ('Everyone', 'FullControl', 'ContainerInherit, ObjectInherit', 'None', 'Allow')
+                $acl.SetAccessRule($accessrule)
+                Set-Acl -Path $_.fullname -AclObject $acl
+            }
+            # permissions set are slightly different for files than directories
+            if(($_).GetType().Name -eq 'FileInfo') {
+                $accessrule = New-Object System.Security.AccessControl.FileSystemAccessRule ('Everyone', 'FullControl', 'ContainerInherit, ObjectInherit', 'InheritOnly', 'Allow')
+                $acl.SetAccessRule($accessrule)
+                Set-Acl -Path $_.fullname -AclObject $acl
+            }
+        }
+
+        #clean up
+        Get-ChildItem C:\Scripts -Recurse | ForEach-Object ($_) {Remove-Item $_.fullname -Recurse}
+        Get-ChildItem C:\data\Scripts -Recurse | ForEach-Object ($_) {Remove-Item $_.fullname -Recurse}
+        Remove-Item C:\Scripts -Recurse -Force
+        Remove-Item C:\data\Scripts -Recurse -Force
+        Remove-Item C:\data\MFCApp_Bin\FirstStartup*.cmd
+        Remove-Item C:\data\MFCApp_Bin\*.vbs
+        Remove-Item C:\data\MFCApp_Bin\*.reg
+        Get-ChildItem C:\data\MFCApp_Bin\Startmenu -Recurse | ForEach-Object ($_) {Remove-Item $_.fullname -Recurse}
+        Remove-Item C:\data\MFCApp_Bin\Startmenu -Recurse -Force
+
+        # run SISU installer, then run SA final install
+        & 'C:\data\MFCApp_Bin\SISU\SISUInstaller.exe' | Out-Default
+    }
     # output to both log file and console for docker logs container_name
     # Tee-Object ensures that install completes before we run W3SVC
     # Setup failed to restart IIS. message at end of installation is OK. W3SVC starts after install completes.
